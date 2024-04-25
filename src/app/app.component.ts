6e29d879e5ad2120d5c0d286a16527f5c214483c
Colin Matteson <seefm@proton.me>
2024-04-25 19:12:40 -0400

    continued work on angular tour of heroes project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HeroesComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
